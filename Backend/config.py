8c725ea812cfdb4e6ab25148076aa17da319e975
OmarFaruk-0x01 <haxzsadik@gmail.com>
2022-04-08 13:06:09 +0600

    MongoClinet URl

--- Backend/config.py
+++ Backend/config.py
@@ -7,4 +7,4 @@ ADMIN_EMAIL: str = "programmer.omar.dev@gmail.com"
 
 
 # mongoClient Url
-MONGO_CLIENT_URL = "mongodb://omar_faruk:Allah_is_1@cluster0-shard-00-00.wkp6f.mongodb.net:27017,cluster0-shard-00-01.wkp6f.mongodb.net:27017,cluster0-shard-00-02.wkp6f.mongodb.net:27017/word_manager?ssl=true&replicaSet=atlas-sxg37s-shard-0&authSource=admin&retryWrites=true&w=majority"
+MONGO_CLIENT_URL = "<mongoclient_url>"
